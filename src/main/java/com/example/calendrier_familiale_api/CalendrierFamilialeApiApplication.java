eda1bccea9835ec6580e14611a68536bc1d5719e
Sullivan Joro <119728329+Sullivan1301@users.noreply.github.com>
2024-09-12 13:56:08 +0300

    chore: intialize the backend project for calendrier familial

---
+++ src/main/java/com/example/calendrier_familiale_api/CalendrierFamilialeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.calendrier_familiale_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendrierFamilialeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendrierFamilialeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calendrier_familiale_api/CalendrierFamilialeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calendrier_familiale_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendrierFamilialeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
