b0d64033b492526643d6a7050bc4f92f0837caf7
Sullivan Joro <119728329+Sullivan1301@users.noreply.github.com>
2025-01-13 19:55:54 +0300

    feat: implement User Family User Repositories

--- src/main/java/com/example/calendrier_familiale_api/repository/EventRepository.java
+++ src/main/java/com/example/calendrier_familiale_api/repository/EventRepository.java
@@ -2,10 +2,7 @@ package com.example.calendrier_familiale_api.repository;
 
 import com.example.calendrier_familiale_api.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
 
-@Repository
-public interface EventRepository extends JpaRepository<Event, UUID> {
+public interface EventRepository extends JpaRepository<Event, Long> {
 }
---
+++ src/main/java/com/example/calendrier_familiale_api/repository/FamilyRepository.java
@@ -0,0 +1,8 @@
+package com.example.calendrier_familiale_api.repository;
+
+import com.example.calendrier_familiale_api.model.Family;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FamilyRepository extends JpaRepository<Family, Long> {
+
+}
